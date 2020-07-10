04fd723b93db1117267d88b4a404246358a7ad0a
yamini465 <40388091+yamini465@users.noreply.github.com>
2020-07-10 11:21:26 +0530

    Updates

---
+++ precache-manifest.b9c3cf2fd2c4e4ce924b04c43f9ffa6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85ddbd9e2a4c946b48e3555496f208f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ef18dd4a1d8bd7904555",
+    "url": "/todolist/static/css/main.e70a4496.chunk.css"
+  },
+  {
+    "revision": "7f8b248f50e2f94feb49",
+    "url": "/todolist/static/js/2.16e61c0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.16e61c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef18dd4a1d8bd7904555",
+    "url": "/todolist/static/js/main.39a09b9c.chunk.js"
+  },
+  {
+    "revision": "130fd48b1b39e8c6b47c",
+    "url": "/todolist/static/js/runtime-main.5f712ceb.js"
+  }
+]);
